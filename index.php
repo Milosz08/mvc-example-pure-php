0be93289de31483dade1182d834853b9fe313fa9
Milosz08 <gilgamilosz451@gmail.com>
2022-11-08 02:17:42 +0100

    create class for loading core files

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 
+use App\Core\CoreLoader;
 use App\Core\MvcApplication;
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+define('__SEP__',           DIRECTORY_SEPARATOR);                           // stała dla domyślnego separatora plików
+define('__SCANNER_DIRS__',  array('core', 'utils', 'models', 'services'));  // katalogi w /src podlegające ładowaniu
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 ob_start(); // włączenie buforowania
 session_start(); //uruchomienie sesji serwera php
 
-// import i stworzenie głównej instancji aplikacji MVC
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'bootstrapper.php';
+// import loadera plików
+require_once __DIR__ . __SEP__ . 'src' . __SEP__ . 'core' . __SEP__ . 'CoreLoader.php';
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+CoreLoader::load(); // instantancja i ładowanie rdzenia aplikacji
+
+// dodatkowe pliki konfiguracyjne
+require_once __DIR__ . __SEP__ . 'src' . __SEP__ . 'config.php';
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
---
+++ src/core/CoreLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Core;
+
+// Klasa przechowująca metody odpowiadające za ładowanie rdzenia aplikacji (plików "core").
+class CoreLoader
+{
+    private static $_instance; // instancja loadera
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    private function __construct()
+    {
+        $this->scan_dirs_and_load();
+    }
+    
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    // Skanowanie wybranych katalogów i ładowanie plików do kontekstu aplikacji.
+    private function scan_dirs_and_load()
+    {
+        $root_dir = realpath(dirname(__FILE__) . '/..'); // katalog główny (przejście stopień wyżej, /src/)
+        foreach (__SCANNER_DIRS__ as $path) // pętla przechodząca przez wszystkie katalogi
+        {
+            // wszystkie pliki w wybranym katalogu z rozszerzeniem php
+            $files_array = glob($root_dir . __SEP__ . $path . __SEP__ . "*.php", GLOB_BRACE);
+            foreach ($files_array as $file) // przejście przez wszystkie pliki katalogu
+            {
+                if ($file !== __FILE__) require_once $file; // jeśli plik nie odnosi się do klasy CoreLoader, załaduj
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    // Metoda uruchamiana przez uruchomieniem głównej metody run z klasy MvcApplication umożliwiająca stworzenie instancji Loadera i w
+    // wyniku tego załadowanie do kontekstu aplikacji plików.
+    public static function load()
+    {
+        if (self::$_instance == null)
+        {
+            new CoreLoader();
+        }
+    }
+}
